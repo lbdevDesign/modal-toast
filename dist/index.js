f0fec3219aef86deb99f10c3fab06b9d71deb18e
Lucas <lucasbombaron@gmail.com>
2023-11-05 12:38:05 +0100

    1.0.14

--- dist/index.js
+++ dist/index.js
@@ -6,9 +6,9 @@ Object.defineProperty(exports, "__esModule", {
 Object.defineProperty(exports, "ModalToast", {
   enumerable: true,
   get: function get() {
-    return _Modal["default"];
+    return _modal["default"];
   }
 });
-var _Modal = _interopRequireDefault(require("./component/Modal"));
+var _modal = _interopRequireDefault(require("./component/modal.js"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6WyJfTW9kYWwiLCJfaW50ZXJvcFJlcXVpcmVEZWZhdWx0IiwicmVxdWlyZSIsIm9iaiIsIl9fZXNNb2R1bGUiXSwic291cmNlcyI6WyIuLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IE1vZGFsVG9hc3QgZnJvbSAnLi9jb21wb25lbnQvTW9kYWwnO1xuXG5leHBvcnQgeyBNb2RhbFRvYXN0IH07XG4iXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7O0FBQUEsSUFBQUEsTUFBQSxHQUFBQyxzQkFBQSxDQUFBQyxPQUFBO0FBQTJDLFNBQUFELHVCQUFBRSxHQUFBLFdBQUFBLEdBQUEsSUFBQUEsR0FBQSxDQUFBQyxVQUFBLEdBQUFELEdBQUEsZ0JBQUFBLEdBQUEifQ==
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6WyJfbW9kYWwiLCJfaW50ZXJvcFJlcXVpcmVEZWZhdWx0IiwicmVxdWlyZSIsIm9iaiIsIl9fZXNNb2R1bGUiXSwic291cmNlcyI6WyIuLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IE1vZGFsVG9hc3QgZnJvbSAnLi9jb21wb25lbnQvbW9kYWwuanMnO1xuXG5leHBvcnQgeyBNb2RhbFRvYXN0IH07XG4iXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7O0FBQUEsSUFBQUEsTUFBQSxHQUFBQyxzQkFBQSxDQUFBQyxPQUFBO0FBQThDLFNBQUFELHVCQUFBRSxHQUFBLFdBQUFBLEdBQUEsSUFBQUEsR0FBQSxDQUFBQyxVQUFBLEdBQUFELEdBQUEsZ0JBQUFBLEdBQUEifQ==
